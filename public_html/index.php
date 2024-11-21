7b8ee91e21eb6b77ee55f26dd992b3872b107fab
JediMB <jedimb@live.se>
2024-11-21 23:53:26 +0100

    Started working on sub-menus

--- public_html/index.php
+++ public_html/index.php
@@ -15,13 +15,29 @@
     <link href="/css/style.css" rel="stylesheet" />
 </head>
 <body>
-    <header class="bg-black px-16 py-8 rounded-full">
-        <nav>
+    <header class="bg-black px-16 py-8 rounded-b-2xl">
+        <nav id="menu-top">
             <ul class="flex gap-2 justify-end">
                 <li><?php menuButton('test', 'Projects') ?></li>
                 <li><?php menuButton('about:blank', 'About me') ?></li>
             </ul>
         </nav>
     </header>
+    <div class="mt-4">
+        <!-- Scrap this and just make drop-down menus -->
+        <nav id="menu-left" class="bg-black w-60 p-8 rounded-2xl">
+            <ul>
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+                <li>Project 4</li>
+                <li>Project 5</li>
+                <li>Project 6</li>
+            </ul>
+        </nav>
+        <main>
+
+        </main>
+    </div>
 </body>
 </html>
