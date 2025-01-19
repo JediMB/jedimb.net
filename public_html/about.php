70cbc1dfa365cee5fc6048207c20f0b5e22b8d31
JediMB <jedimb@live.se>
2025-01-19 01:55:48 +0100

    Limited navigation menu styling to elements within the menu element, and started writing the "About me" page

---
+++ public_html/about.php
@@ -0,0 +1,32 @@
+<?php
+    setPageTitle('About me');
+    setCopyrightYearByFile(__FILE__);
+?>
+
+<page-content>
+    <main>
+        <h2>About me</h2>
+        <p>
+            I'm a Swedish 🇸🇪 programmer <em>(<a href="https://cv.jedimb.net" target="_blank">actively looking for work</a>)</em> with a passion
+            for coding that I've thankfully been able to reignite in recent years, after a burnout episode set me back for a while. Being born
+            and raised in Sweden, I'm naturally fluent in Swedish, but I'm comfortable with and tend to default to English due to practically
+            growing up on the Internet since my teenage years.
+        </p>
+        <p>
+            Online or off, I've always been a <i>"tech wiz"</i> who's been quick to adapt to and learn new things. I've used <i>MS-DOS</i>
+            and every major <i>Microsoft Windows</i> release from <i>Windows 3.1</i> through <i>Windows 10</i>, and the only reason I didn't
+            upgrade to <i>Windows 11</i> years ago is because I haven't had the means to upgrade my computer to one that meets the arbitrary
+            CPU and <a href="https://en.wikipedia.org/wiki/Trusted_Platform_Module" target="_blank"><i>TPM 2.0</i></a> requirements.
+        </p>
+        <p>
+            But why not turn that misfortune into an opportunity to try something else instead?
+        </p>
+        <p>
+            Since December 7, 2024, I've practically speaking been a full-time Linux user instead. While I'm not opposed to using Windows
+            operating systems and keep my old installation on a separate partition for when I might need to boot into it, I've gained a
+            growing appreciation for <a href="https://en.wikipedia.org/wiki/Free_and_open-source_software" target="_blank"><i>FOSS</i></a>
+            in recent years. At the moment, I'm using the very convenient <i>Debian 12</i> on my desktop PC while a home server I maintain
+            runs the Debian edition of <i>Linux Mint.</i>
+        </p>
+    </main>
+</page-content>
