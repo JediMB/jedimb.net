1328b2a34f70fef30093c5f65d3486752598d65d
JediMB <jedimb@live.se>
2025-01-22 22:44:43 +0100

    New font, tweaked css, more About text

--- public_html/about.php
+++ public_html/about.php
@@ -7,26 +7,69 @@
     <main>
         <h2>About me</h2>
         <p>
-            I'm a Swedish 🇸🇪 programmer <em>(<a href="https://cv.jedimb.net" target="_blank">actively looking for work</a>)</em> with a passion
-            for coding that I've thankfully been able to reignite in recent years, after a burnout episode set me back for a while. Being born
-            and raised in Sweden, I'm naturally fluent in Swedish, but I'm comfortable with and tend to default to English due to practically
-            growing up on the Internet since my teenage years.
+            I'm a Swedish 🇸🇪 programmer <em>(<a href="https://cv.jedimb.net" target="_blank">actively looking for work</a>)</em>
+            with a passion for coding that, after a burnout episode in the past, I've thankfully been able to reignite and nurture in recent years.
+            Being born and raised in and around Gävle, Sweden, I'm naturally fluent in Swedish, but I'm comfortable with and tend to default to
+            English due to practically growing up on the Internet since my teenage years.
         </p>
+
+        <h3>PC and Me</h3>
+        <!--template-->
         <p>
-            Online or off, I've always been a <i>"tech wiz"</i> who's been quick to adapt to and learn new things. I've used <i>MS-DOS</i>
-            and every major <i>Microsoft Windows</i> release from <i>Windows 3.1</i> through <i>Windows 10</i>, and the only reason I didn't
-            upgrade to <i>Windows 11</i> years ago is because I haven't had the means to upgrade my computer to one that meets the arbitrary
-            CPU and <a href="https://en.wikipedia.org/wiki/Trusted_Platform_Module" target="_blank"><i>TPM 2.0</i></a> requirements.
+            Online or off, I've always been a <i>"computer wiz"</i> who's been quick to adapt to and learn new things. I've used <i>MS-DOS</i>
+            and every major Microsoft Windows home release from <i>Windows 3.1</i> through <i>Windows 10</i>, and assembled or upgraded
+            my own desktop PCs since I was 13 years old. The only reason I didn't upgrade to <i>Windows 11</i> years ago is because I haven't had
+            the means (or need) to upgrade my computer's hardware to meet Microsoft's arbitrary CPU and
+            <i><a href="https://en.wikipedia.org/wiki/Trusted_Platform_Module" target="_blank">TPM 2.0</a></i> requirements.
         </p>
         <p>
             But why not turn that misfortune into an opportunity to try something else instead?
         </p>
         <p>
-            Since December 7, 2024, I've practically speaking been a full-time Linux user instead. While I'm not opposed to using Windows
+            Since December 7, 2024, I've practically speaking been a full-time Linux user. While I'm not opposed to using Windows
             operating systems and keep my old installation on a separate partition for when I might need to boot into it, I've gained a
-            growing appreciation for <a href="https://en.wikipedia.org/wiki/Free_and_open-source_software" target="_blank"><i>FOSS</i></a>
+            growing appreciation for <i><a href="https://en.wikipedia.org/wiki/Free_and_open-source_software" target="_blank">FOSS</a></i>
             in recent years. At the moment, I'm using the very convenient <i>Debian 12</i> on my desktop PC while a home server I maintain
             runs the Debian edition of <i>Linux Mint.</i>
+        </p>
+        <p>
+            Presumably I'll switch to <i>Arch Linux</i> at some point in the future, but right now I have other things to focus on.
+        </p>
+        <!--/template-->
+        
+        <h3>Coding, Pt. 1</h3>
+
+        <p>
+            While not programming per se, I had my first encounter with HTML and CSS as a child in 1997, when a fresh, new computer magazine called
+            <i><a href="https://sv.wikipedia.org/wiki/PC_f%C3%B6r_alla" target="_blank">PC för alla</a></i> (lit. "PC for everyone") included tutorials
+            and tips on the basics of both, at a time when CSS in particular was something brand new and exciting (but with no best practices).
+            Subsequently, I set up a free website on Angelfire that I briefly used to host fanfiction, before getting my account shut down for bypassing
+            their off-site linking restrictions. 🤷🏻‍♀️
+        </p>
+        <p>
+            It was very much a different time.
+        </p>
+        <p>
+            During my upper secondary education, focused on computer technology, I got to dip into actual programming, although further experience
+            showed me just how disorganized and inadequate that course was. We learned some basics of <nobr>C++</nobr>, Visual Basic and Java, and then
+            a few of us moved on to make a side-scrolling space <i><a href="https://en.wikipedia.org/wiki/Shoot_%27em_up" target="_blank">shmup</a></i>
+            using <nobr>C++</nobr> and a DirectX wrapper. There were a lot of very obvious gaps in our knowledge, though.
+        </p>
+        <p>
+            Amusingly, my most vivid memory of all of this was one of the teachers pointing at the code he'd written on a whiteboard, stating that
+            <i>"this is a pointer"</i> and giving no elaboration on what a pointer even was in the context of programming.
+        </p>
+        <p>
+            As my upper secondary education ended, I was... tired, to say the least. I wanted to take a year off from studying to focus on something
+            else, but a classmate convinced me to come along for an interview at a school focused in video game development. While I learned a lot,
+            and actually got a better foundation of <nobr>C++</nobr> knowledge, I ended up not being able to keep up and never finished several classes.
+        </p>
+
+        <h3>Coding, Pt. 2</h3>
+
+        <p>
+            A few years and some misadventures in Ireland later, I ended up getting a programming internship at a company in Stockholm. It started out
+            with some quick courses 
         </p>
     </main>
 </page-content>
