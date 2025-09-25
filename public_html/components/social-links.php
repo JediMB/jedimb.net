72740a34d1acc78acc114396eb33394f0e964289
JediMB <jedimb@live.se>
2025-09-25 14:50:43 +0200

    Restructured social links component

--- public_html/components/social-links.php
+++ public_html/components/social-links.php
@@ -2,60 +2,36 @@
 
 require_once 'services/social-link.service.php';
 
+use Models\SocialLink;
 use Services\SocialLinkService;
 
-function socialLinks() {
-    $socials = SocialLinkService::getInstance()->getSocialLinks();
-    if (count($socials) === 0) return;
-
-    $unsuffixedComponentPath = rtrim(__FILE__, 'php');
-    $cssPath = realpath($unsuffixedComponentPath . 'css');
-    $jsPath = realpath($unsuffixedComponentPath . 'js')
-        ? substr($unsuffixedComponentPath, strlen(getcwd())) . 'js'
-        : false;
-
-    echo $cssPath ? '<style type="text/css">' . file_get_contents($cssPath) . '</style>' : null;
-    echo $jsPath ? '<script src="'. $jsPath . '" defer></script>' : null;
-
-    echo '<svg class="hidden" xmlns="http://www.w3.org/2000/svg">';
-    foreach($socials as $social) {
-        /** @var SocialLink $social */
-        try {
-            $symbol = <<<HTML
-                <symbol
-                    id="svg-social-link-{$social->id}"
-                    width="2rem"
-                    height="2rem"
-                    viewBox="{$social->svgViewBox}"
-                    fill="inherit">
-            HTML;
-
-            $content = $social->svgContent;
-
-            echo $symbol . $content . '</symbol>';
-        }
-        catch (Exception) {
-            continue;
-        }
-    }
-    echo '</svg>';
-
-    foreach($socials as $social) {
-        /** @var SocialLink $social */
-        try {
-            echo <<<HTML
-                <a href="{$social->url}" title="{$social->description}" target="_blank" class="social-link"
-                    aria-label="Social link to {$social->description}">
-                    <svg width="2rem" height="2rem">
-                        <use xlink:href="#svg-social-link-{$social->id}" href="#svg-social-link-{$social->id}"></use>
-                    </svg>
-                </a>
-            HTML;
-        }
-        catch (Exception) {
-            continue;
-        }
-    }
-}
+$socials = SocialLinkService::getInstance()->getSocialLinks();
+$symbolPrefix = 'svg-social-link-';
 
 ?>
+
+<svg class="hidden" xmlns="http://www.w3.org/2000/svg">
+    <?php foreach ($socials as $link): ?>
+        <?php /** @var SocialLink $link */ ?>
+        <symbol
+            id="<?= $symbolPrefix . $link->id ?>"
+            width="2rem" height="2rem"
+            viewBox="<?= $link->svgViewBox ?>"
+            fill="inherit">
+            <?= $link->svgContent ?>
+        </symbol>
+    <?php endforeach ?>
+</svg>
+<?php foreach ($socials as $link): ?>
+    <?php $href = "#$symbolPrefix" . $link->id ?>
+    <a href="<?= $link->url ?>"
+        title="<?= $link->description ?>"
+        target="_blank"
+        class="social-link"
+        aria-label="Social link to <?= $link->description ?>">
+        <svg width="2rem" height="2rem">
+            <use xlink:href="#<?= $href ?>"
+                href="<?= $href ?>"></use>
+        </svg>
+    </a>
+<?php endforeach ?>
