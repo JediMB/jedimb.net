89625557a8be739db3c7866a2a52e7a22c5f48e3
JediMB <jedimb@live.se>
2025-09-26 10:30:09 +0200

    Tweaked login CSS

--- public_html/account/login.php
+++ public_html/account/login.php
@@ -8,10 +8,16 @@ if (isset($_SESSION['account_loggedin'])) {
 
 <div>X-Men! Welcome to login!</div>
 
-<form action="/api/auth" method="post" class="flex flex-col">
-    <label for="username">User name</label>
-    <input type="text" name="username" id="username" placeholder="User name" required>
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password" placeholder="Password" required>
-    <button type="submit">Login</button>
-</form>
+<div class="w-80 mx-auto">
+    <form action="/api/auth" method="post" class="flex flex-col gap-3">
+        <div>
+            <label for="username">User name</label>
+            <input type="text" name="username" id="username" placeholder="User name" required class="w-full p-1 text-black">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Password" required class="w-full p-1 text-black">
+        </div>
+        <button type="submit" class="btn hover:bg-hotpink-500 hover:text-black">Login</button>
+    </form>
+</div>
