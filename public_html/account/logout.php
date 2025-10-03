289d922ace01478dd97bdf6d9b0f383fd89f680b
JediMB <jedimb@live.se>
2025-10-03 10:41:54 +0200

    Updated page-based logout to clear cookies

--- public_html/account/logout.php
+++ public_html/account/logout.php
@@ -9,6 +9,11 @@ use Services\SessionService;
 $service = SessionService::getInstance(); /** @var SessionService $service */
 $service->clearSession();
 
+$pastExpiration = time()-86400;
+setcookie(COOKIE_USER_KEY, '', $pastExpiration);
+setcookie(COOKIE_TOKEN_KEY, '', $pastExpiration);
+setcookie(COOKIE_VALIDATOR_KEY, '', $pastExpiration);
+
 header('Location: /');
 
 ?>
