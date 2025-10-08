fa3e66163ea62841ea4b364e07424822c31943a0
JediMB <jedimb@live.se>
2025-10-08 14:24:23 +0200

    New CSS, cookie keys from meta tags used, input messages moved into data attributes, constant use updated, input-error attribute added to field-specific error containers

--- public_html/pages/account/login.php
+++ public_html/pages/account/login.php
@@ -19,23 +19,27 @@ if (isset($_SESSION[SESSION_STATUS_KEY])) {
             <label for="username">Username</label>
             <input type="text" name="username" id="username" placeholder="Username"
                 pattern="<?= trim(REGEX_INPUT['username'], '/') ?>" required
+                data-too-short="Username too short: <?= TEXT_USERNAME_LENGTH ?>"
+                data-too-long="Username too long: <?= TEXT_USERNAME_LENGTH ?>"
                 data-mismatch="<?= TEXT_USERNAME_CHARS ?>"
                 minlength="<?= INPUT_LENGTH['username']['min'] ?>"
                 maxlength="<?= INPUT_LENGTH['username']['max'] ?>"
                 title="<?= TEXT_USERNAME_LENGTH . ' ' . TEXT_USERNAME_CHARS ?>"
                 class="w-full p-1 text-black">
-            <div></div>
+            <div input-error></div>
         </div>
         <div>
             <label for="password">Password</label>
             <input type="password" name="password" id="password" placeholder="Password"
                 pattern="<?= trim(REGEX_INPUT['password'], '/') ?>" required
+                data-too-short="Password too short: <?= TEXT_PASSWORD_LENGTH ?>"
+                data-too-long="Password too long: <?= TEXT_PASSWORD_LENGTH ?>"
                 data-mismatch="<?= TEXT_PASSWORD_CHARS ?>"
                 minlength="<?= INPUT_LENGTH['password']['min'] ?>"
                 maxlength="<?= INPUT_LENGTH['password']['max'] ?>"
                 title="<?= TEXT_PASSWORD_LENGTH . ' ' . TEXT_PASSWORD_CHARS ?>"
                 class="w-full p-1 text-black">
-            <div></div>
+            <div input-error></div>
         </div>
         <div>
             <input type="checkbox" name="rememberme" id="rememberme">
@@ -55,11 +59,20 @@ if (isset($_SESSION[SESSION_STATUS_KEY])) {
     const inputs = form.querySelectorAll('[pattern]');
     const loginBtn = form.querySelector('[type=submit]');
     const errorContainer = document.querySelector('#login-errors');
+    const cookieKeys = [
+        document.querySelector('meta[name="cookie-user-key"]'),
+        document.querySelector('meta[name="cookie-token-key"]'),
+        document.querySelector('meta[name="cookie-validator-key"]')
+    ];
 
-    // TODO: Button [disabled] CSS, keep the button disabled if fields are invalid
+    const addErrorMessage = (element, message) => {
+        const newError = document.createElement('div');
+        newError.classList.add('error');
+        newError.innerHTML = message;
+        element.appendChild(newError);
+    };
 
     const login = async (event) => {
-        // TODO: On-submit form validation (RegEx?)
         event.submitter.disabled = true;
         errorContainer.textContent = '';
         const formData = new FormData(form);
@@ -69,41 +82,31 @@ if (isset($_SESSION[SESSION_STATUS_KEY])) {
         if (data.success) {
             if (data.value.token) {
                 const expires = data.value.expiresOn.toUTCString();
-                document.cookie = `<?= COOKIE_USER_KEY ?>=${data.value.userId}; expires=${expires};`
-                document.cookie = `<?= COOKIE_TOKEN_KEY ?>=${data.value.token}; expires=${expires};`
-                document.cookie = `<?= COOKIE_VALIDATOR_KEY ?>=${data.value.validator}; expires=${expires};`;
+                document.cookie = `${cookieKeys[0].content}=${data.value.userId}; expires=${expires};`
+                document.cookie = `${cookieKeys[1].content}=${data.value.token}; expires=${expires};`
+                document.cookie = `${cookieKeys[2].content}=${data.value.validator}; expires=${expires};`;
             }
             
             setTimeout(() => window.location.href = '/', 5000);
             return;
         }
 
-        data.errors.forEach(error => {
-            const newError = document.createElement('div');
-            newError.classList.add('error');
-            newError.textContent = error;
-            errorContainer.appendChild(newError);
-        });
+        errorContainer.innerHTML = '';
+        data.errors.forEach(error => addErrorMessage(errorContainer, error));
 
         event.submitter.disabled = false;
     }
-
-    const addErrorMessage = (element, message) => {
-        const newError = document.createElement('div');
-        newError.classList.add('error');
-        newError.textContent = message;
-        element.appendChild(newError);
-    };
 
     const validateForm = (source, eventType) => {
         let isValid = true;
 
         inputs.forEach(input => {
             if (input.checkValidity()) {
-                if (eventType === 'input' && source === input && input.nextElementSibling) {
+                if (eventType === 'input' && source === input) {
                     input.classList.remove('error');
-                console.log(input.validity);
-                    input.nextElementSibling.innerHTML = '';
+
+                    if (input.nextElementSibling?.hasAttribute('input-error'))
+                        input.nextElementSibling.innerHTML = '';
                 }
 
                 return;
@@ -112,16 +115,14 @@ if (isset($_SESSION[SESSION_STATUS_KEY])) {
             if (eventType === 'change' && source === input) {
 
                 input.classList.add('error');
-                console.log(input.validity);
                 
-                // TODO: Verify that the sibling is actually the desired element
-                if (input.nextElementSibling) {
+                if (input.nextElementSibling?.hasAttribute('input-error')) {
                     input.nextElementSibling.textContent = '';
-
+                    
                     if (input.validity.tooShort)
-                        addErrorMessage(input.nextElementSibling, '<?= TEXT_INPUT_TOOSHORT ?>');
+                        addErrorMessage(input.nextElementSibling, input.dataset.tooShort);
                     else if (input.validity.tooLong)
-                        addErrorMessage(input.nextElementSibling, '<?= TEXT_INPUT_TOOLONG ?>');
+                        addErrorMessage(input.nextElementSibling, input.dataset.tooLong);
                     if (input.validity.patternMismatch)
                         addErrorMessage(input.nextElementSibling, input.dataset.mismatch);
                 }
