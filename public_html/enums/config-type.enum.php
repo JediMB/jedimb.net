c96d045bba3d7f97092b3cdd2d64742b23f01176
JediMB <jedimb@live.se>
2025-10-20 16:46:12 +0200

    Removed ConfigType enum and associated  columns/members from Configuration model, as the table will only be used for text strings

--- public_html/enums/config-type.enum.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Enums;
-
-enum ConfigType : int {
-    case string = 0;
-}
-
-?>
--- public_html/models/db/configuration.db.model.php
+++ public_html/models/db/configuration.db.model.php
@@ -2,17 +2,13 @@
 
 namespace Models\DB;
 
-require_once 'enums/config-type.enum.php';
 require_once 'models/base/db-created-modified.model.php';
 
-use Enums\ConfigType;
 use Models\Base\DBCreatedModified;
 
 class Configuration extends DBCreatedModified {
     public string $name;
     public string $value;
-    public ConfigType $type;
-    public string $typeString;
     public bool $isActive;
 
     public function __construct(array $dbRow) {
@@ -20,8 +16,6 @@ class Configuration extends DBCreatedModified {
 
         $this->name = $dbRow['name'];
         $this->value = $dbRow['value'];
-        $this->type = ConfigType::tryFrom($dbRow['type']) ?: ConfigType::string;
-        $this->typeString = $dbRow['type_string'];
         $this->isActive = $dbRow['is_active'];
     }
 }
