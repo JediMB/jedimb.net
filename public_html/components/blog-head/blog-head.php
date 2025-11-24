132e8ca2f0d487842c2224fb8bb2ce325f43820e
JediMB <jedimb@live.se>
2025-11-24 13:53:30 +0100

    Added input fields for title and Facebook URL and tweaked textbox CSS

--- public_html/components/blog-head/blog-head.php
+++ public_html/components/blog-head/blog-head.php
@@ -16,4 +16,6 @@ Component::addJSModule(__FILE__);
     <svg id="svg-blog-add" width="2em" height="2em" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
 </button>
 
+<input type="text" placeholder="Title">
+<input type="url" placeholder="Facebook link">
 <?php Component::include('text-editor')  ?>
