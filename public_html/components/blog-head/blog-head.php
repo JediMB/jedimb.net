efdef6a1bc786e284f67cfc6039b0b015a23b16e
JediMB <jedimb@live.se>
2025-11-26 15:37:18 +0100

    Added permission enforcement for Blog Head and Image Gallery components

--- public_html/components/blog-head/blog-head.php
+++ public_html/components/blog-head/blog-head.php
@@ -4,7 +4,12 @@ namespace Components;
 
 require_once 'utilities/component.utility.php';
 
+use Enums\UserPermission;
+use Services\SessionService;
 use Utilities\Component;
+
+$sessionService = SessionService::getInstance(); /** @var SessionService $sessionService */
+$sessionService->enforcePermissions([UserPermission::Publishing]);
 
 Component::renderOnce();
 Component::renderCSS();
