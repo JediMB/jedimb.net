27dc52ae88794c21ea8502998cc5bd559b42f6d2
JediMB <jedimb@live.se>
2025-12-03 17:03:37 +0100

    Started editing/viewing functionality in image manager

--- public_html/components/image-manager/image-manager.module.js
+++ public_html/components/image-manager/image-manager.module.js
@@ -3,8 +3,10 @@ import imageManagerService from "/js/services/image-manager.service.js";
 customElements.define('image-manager-component', class ImageManagerComponent extends HTMLElement {
     /** @type {ImageManagerComponent} */
     #self;
-    #container;
     #service;
+    #properties;
+    /** @type HTMLTemplateElement */
+    #propertiesTemplate;
     
     constructor() {
         const component = super();
@@ -14,14 +16,27 @@ customElements.define('image-manager-component', class ImageManagerComponent ext
 
     connectedCallback() {
         const self = this.#self;
-        this.#container = this.querySelector('images-container');
 
-        // const tabs = self.querySelectorAll('manager-tabs > input[type="radio"]');
-        // for (const tab of tabs) {
-        //     tab.addEventListener('change', (event) => {
-        //         event.stopPropagation();
+        const fileList = self.querySelector('manager-files');
+        this.#properties = self.querySelector('manager-properties');
+        this.#propertiesTemplate = self.querySelector('manager-properties + template');
 
-        //     })
-        // }
+        fileList.addEventListener('change', event => {
+            if (!event.target.checked)
+                return;
+
+            event.stopPropagation();
+
+
+            console.log(event.target);
+
+            const template = this.#propertiesTemplate.content.cloneNode(true);
+            this.#properties.textContent = '';
+            this.#properties.appendChild(template);
+        });
+    }
+
+    #updateManagerProperties() {
+
     }
 });
--- public_html/components/image-manager/image-manager.php
+++ public_html/components/image-manager/image-manager.php
@@ -42,6 +42,7 @@ Component::addJSModule();
                             <label>
                                 <input hidden type="radio" name="images"
                                     data-image-id="<?= $image->id ?>"
+                                    data-image-filename="<?= $image->filename ?>"
                                     data-image-url="<?= '/' . PATH_IMAGE_GALLERY . "/$image->filename" ?>"
                                     data-image-title="<?= htmlspecialchars($image->title) ?>"
                                     data-image-description="<?= htmlspecialchars($image->description) ?>"
@@ -72,12 +73,32 @@ Component::addJSModule();
                     </div>
                     <div>
                         <label for="description">Description:</label>
-                        <textarea full-width name="description" rows="3">Description: The text that goes in the ALT field</textarea>
+                        <textarea full-width name="description" rows="3">The text that goes in the ALT field</textarea>
                     </div>
                     <div>Created on: 2025-11-27 13:45 (UTC+1)</div>
                     <div>Modified on: (unmodified)</div>
                 </form>
             </div>
         </manager-properties>
+        <template>
+            <h3 class="image-header">Filename</h3>
+            <div class="image-preview">
+                &nbsp;
+            </div>
+            <div>
+                <form>
+                    <div>
+                        <label for="title">Title:</label>
+                        <input full-width type="text" name="title" value="Title">
+                    </div>
+                    <div>
+                        <label for="description">Description:</label>
+                        <textarea full-width name="description" rows="3">Description</textarea>
+                    </div>
+                    <div>Created on: 2025-11-27 13:45 (UTC+1)</div>
+                    <div>Modified on: (unmodified)</div>
+                </form>
+            </div>
+        </template>
     </manager-content>
 </image-manager>
