833dce70e0919b9e9ff27fbe04b4465259b5c29a
JediMB <jedimb@live.se>
2025-12-08 19:10:42 +0100

    Modified Emitter and Listener JS classes to also handle arrays with changes/subscriptions to values within them

--- public_html/js/utilities/emitter.js
+++ public_html/js/utilities/emitter.js
@@ -8,7 +8,29 @@ export default class Emitter {
 
     getValue() { return this.#value; }
 
-    setValue(value) {
+    /**
+     * Sets the entire value/object, or a value within an array
+     * 
+     * @param {any} value 
+     * @param {(Number|undefined)} arrayIndex 
+     */
+    setValue(value, arrayIndex = undefined) {
+        if (typeof arrayIndex === 'number') {
+            if (!Array.isArray(this.#value))
+                throw new Error('Index provided for non-array');
+
+            if (this.#value[arrayIndex] === value)
+                return;
+
+            this.#value[arrayIndex] = value;
+
+            this.#listeners.forEach(listener => {
+                listener.nextIndexed(this.#value[arrayIndex]);
+            });
+
+            return;
+        }
+
         if (this.#value === value)
             return;
 
@@ -19,8 +41,16 @@ export default class Emitter {
         });
     }
 
-    subscribe(next /*{ next, error, complete }*/, getCurrentValue = false) {
-        const listener = new Listener(this, next /*{ next, error, complete }*/);
+    /**
+     * Subscribes to changes of the whole value/object, to values within an array, or both
+     * 
+     * @param {((value: any) => void|undefined)} next
+     * @param {Boolean} getCurrentValue 
+     * @param {((index: Number, value: any) => void|undefined)} nextIndexed
+     * @returns {Listener}
+     */
+    subscribe(next = undefined /*{ next, error, complete }*/, getCurrentValue = false, nextIndexed = undefined) {
+        const listener = new Listener(this, next, nextIndexed /*{ next, error, complete }*/);
         this.#listeners.push(listener);
 
         if (getCurrentValue)
@@ -29,6 +59,7 @@ export default class Emitter {
         return listener;
     }
 
+    /** @param {Listener} listener */
     unsubscribe(listener) {
         this.#listeners = this.#listeners.filter(sub => sub !== listener);
     }
@@ -37,17 +68,33 @@ export default class Emitter {
 export class Listener {
     #emitter;
     #onNext;
+    #onNextIndexed;
 
-    constructor(emitter, next /*{ next, error, complete }*/) {
+    /**
+     * @param {Emitter} emitter 
+     * @param {(Function|undefined)} next 
+     * @param {(Function|undefined)} nextIndexed 
+     */
+    constructor(emitter, next = undefined, nextIndexed = undefined /*{ next, error, complete }*/) {
         this.#emitter = emitter;
 
         if (next && typeof next === 'function')
             this.#onNext = next;
+
+        if (nextIndexed && typeof nextIndexed === 'function')
+            this.#onNextIndexed = nextIndexed;
     }
 
     unsubscribe() {
         this.#emitter.unsubscribe(this);
     }
 
+    /** @param {any} value  */
     next(value) { this.#onNext?.call(this, value); }
+
+    /**
+     * @param {Number} index
+     * @param {any} value 
+     */
+    nextIndexed(index, value) { this.#onNextIndexed?.call(this, index, value); }
 }
