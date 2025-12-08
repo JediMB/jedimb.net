d4dde5ea27ae4978d7d5104f94c8b35ad3d6b89b
JediMB <jedimb@live.se>
2025-12-08 19:15:14 +0100

    Update functionality added to image gallery service and API

--- public_html/api/images.php
+++ public_html/api/images.php
@@ -1,15 +1,22 @@
 <?php declare(strict_types=1);
 
+require_once 'models/dto/image.dto.model.php';
 require_once 'services/image-gallery.service.php';
 require_once 'utilities/response.utility.php';
 
+use Enums\UserPermission;
+use Models\DTO\Image;
 use Services\ImageGalleryService;
+use Services\SessionService;
 use Utilities\Response;
 
 $service = ImageGalleryService::getInstance(); /** @var ImageGalleryService $service */
+$sessionService = SessionService::getInstance(); /** @var SessionService $sessionService */
 
 if (isset($GLOBALS['api_params'][0]))
     $id = (int)($GLOBALS['api_params'][0]);
+
+$input = json_decode(file_get_contents('php://input'), true);
 
 switch ( $_SERVER['REQUEST_METHOD'] ) {
     case 'GET':
@@ -28,8 +35,35 @@ switch ( $_SERVER['REQUEST_METHOD'] ) {
             return Response::Error([$e->getMessage()]);
         }
 
+    case 'PATCH':
+        conditionChecks([ UserPermission::Editing ]);
+        
+        try {
+            $imageDTO = new Image($input);
+
+            $result = $service->updateImage($imageDTO);
+
+            return Response::Success($result);
+        }
+        catch (Exception $e) {
+            return Response::Error([$e->getMessage()]);
+        }
+
     default:
         return Response::InvalidRequest();
+}
+
+function conditionChecks(array $permissions) {
+    global $sessionService, $input; /** @var SessionService $sessionService */
+    
+    if (!$sessionService->isLoggedIn())
+        return Response::Forbidden(TEXT_NOT_LOGGED_IN);
+
+    if (!$sessionService->hasPermissions($permissions))
+        return Response::Forbidden(TEXT_INSUFFICIENT_PERMISSIONS);
+
+    if (empty($input))
+        return Response::BadRequest('Request body is empty');
 }
 
 ?>
--- public_html/services/image-gallery.service.php
+++ public_html/services/image-gallery.service.php
@@ -2,19 +2,25 @@
 
 namespace Services;
 
+require_once 'models/dto/image.dto.model.php';
+require_once 'services/table-modified.service.php';
 require_once 'services/base/singleton.php';
 require_once 'services/db/image-gallery.db.service.php';
 
 use Models\DB\Gallery;
 use Models\DB\Image;
+use Models\DTO\Image as ImageDTO;
+use Services\TableModifiedService;
 use Services\Base\Singleton;
 use Services\DB\ImageGalleryDBService;
 
 class ImageGalleryService extends Singleton {
     private ImageGalleryDBService $imageGalleryDbService;
+    private TableModifiedService $tableModifiedService;
 
     protected function __construct() {
         $this->imageGalleryDbService = ImageGalleryDBService::getInstance();
+        $this->tableModifiedService = TableModifiedService::getInstance();
     }
 
     public function getGalleries() : array {
@@ -31,6 +37,20 @@ class ImageGalleryService extends Singleton {
 
     public function getImages() : array {
         return $this->imageGalleryDbService->getImages();
+    }
+
+    /** @return array{'object': \Models\DB\Image, 'modifiedOn': \DateTime} */
+    public function updateImage(ImageDTO $imageDTO) : array {
+        $image = $this->imageGalleryDbService->getImage($imageDTO->id);
+        ImageDTO::update($image, $imageDTO);
+
+        $updatedImage = $this->imageGalleryDbService->updateImage($image);
+        $modifiedOn = $this->tableModifiedService->createOrUpdateTableModifiedDate('image');
+
+        return [
+            'object' => $updatedImage,
+            'modifiedOn' => $modifiedOn
+        ];
     }
 }
 
