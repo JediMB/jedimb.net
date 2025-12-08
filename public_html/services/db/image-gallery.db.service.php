dc642c9833ea446f7468c65288c3915bee198491
JediMB <jedimb@live.se>
2025-12-08 19:14:17 +0100

    Create and Update functionality added to image gallery DB service

--- public_html/services/db/image-gallery.db.service.php
+++ public_html/services/db/image-gallery.db.service.php
@@ -7,9 +7,11 @@ require_once 'models/db/image.db.model.php';
 require_once 'services/base/base.db.service.php';
 
 use Exception;
+use PDO;
 use Models\DB\Gallery;
 use Models\DB\GalleryImage;
 use Models\DB\Image;
+use Models\DTO\Image as ImageDTO;
 use PDOException;
 use Services\Base\BaseDBService;
 
@@ -96,6 +98,40 @@ class ImageGalleryDBService extends BaseDBService {
             }
 
             return $groupedImages;
+        }
+        catch (PDOException $e) {
+            throw new Exception('Database error: ' . $e->getMessage());
+        }
+    }
+
+    public function createImage(ImageDTO $object) : Image {
+        try {
+            $result = $this->dbService->selectFunction(
+                'create_image', [
+                    1 => [ 'value' => $object->filename, 'type' => PDO::PARAM_STR ],
+                    2 => [ 'value' => $object->title, 'type' => PDO::PARAM_STR ],
+                    3 => [ 'value' => $object->description, 'type' => PDO::PARAM_STR ]
+                ]
+            );
+
+            return new Image($result);
+        }
+        catch (PDOException $e) {
+            throw new Exception('Database error: ' . $e->getMessage());
+        }
+    }
+
+    public function updateImage(Image $object) : Image {
+        try {
+            $result = $this->dbService->selectFunction(
+                'update_image', [
+                    1 => [ 'value' => $object->id, 'type' => PDO::PARAM_INT ],
+                    2 => [ 'value' => $object->title, 'type' => PDO::PARAM_STR ],
+                    3 => [ 'value' => $object->description, 'type' => PDO::PARAM_STR ]
+                ]
+            );
+
+            return new Image($result);
         }
         catch (PDOException $e) {
             throw new Exception('Database error: ' . $e->getMessage());
