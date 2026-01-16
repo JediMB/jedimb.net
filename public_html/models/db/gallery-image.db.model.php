a2d1c487b25de79d1a617088989998c2a463833d
JediMB <jedimb@live.se>
2026-01-16 22:02:56 +0100

    Added create and match functions to GalleryImage DB model

--- public_html/models/db/gallery-image.db.model.php
+++ public_html/models/db/gallery-image.db.model.php
@@ -18,6 +18,24 @@ class GalleryImage extends DBBase {
         $this->imageId = $dbRow['image_id'];
         $this->order = $dbRow['order'];
     }
+
+    public static function create($galleryId, $imageId, $order) {
+        return new GalleryImage([
+                'id' => 0,
+                'gallery_id' => $galleryId,
+                'image_id' => $imageId,
+                'order' => $order
+        ]);
+    }
+
+    public function match(GalleryImage $other, bool $matchId = false) : bool {
+        if ($matchId && $other->id !== $this->id)
+            return false;
+
+        return $other->galleryId === $this->galleryId
+            && $other->imageId === $this->imageId
+            && $other->order === $this->order;
+    }
 }
 
 ?>
