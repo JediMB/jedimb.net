ee039d1b193fd71d8cb48e2cfe2d41cd15b98e54
JediMB <jedimb@live.se>
2026-01-19 16:55:10 +0100

    Removed console.log comment

--- public_html/components/site-configuration/site-configuration.module.js
+++ public_html/components/site-configuration/site-configuration.module.js
@@ -48,8 +48,7 @@ class SiteConfiguration {
         if (updatedConfigs.length > 0)
             responses.push(this.#configApiService.updateConfigurations(updatedConfigs));
 
-        Promise.all(responses).then(response => {
-            // response.forEach(r => console.log(r));
+        Promise.all(responses).then(() => {
             setTimeout(() => location.reload(), 1000);
         })
     }
