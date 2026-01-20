d86badb81ea62ef58a578f58fc4829b18e89eb45
JediMB <jedimb@live.se>
2026-01-20 13:14:14 +0100

    Added JSDoc to gallery and image models

--- public_html/js/models/image-gallery/gallery.model.js
+++ public_html/js/models/image-gallery/gallery.model.js
@@ -1,4 +1,13 @@
 export default class Gallery {
+    /**
+     * @param {Object} param0 
+     * @param {number} param0.id
+     * @param {string} param0.title
+     * @param {string} param0.description
+     * @param {{date: string, timezone: string}} param0.createdOn
+     * @param {?{date: string, timezone: string}} param0.modifiedOn
+     * @param {number[]} param0.imageIds
+     */
     constructor({id, title, description, createdOn, modifiedOn, imageIds}) {
         this.id = Number(id);
         this.title = title;
--- public_html/js/models/image-gallery/image.model.js
+++ public_html/js/models/image-gallery/image.model.js
@@ -1,4 +1,14 @@
 export default class Image {
+    /**
+     * @param {Object} param0 
+     * @param {number} param0.id
+     * @param {string} param0.filename
+     * @param {string} param0.title
+     * @param {string} param0.description
+     * @param {{date: string, timezone: string}} param0.createdOn
+     * @param {?{date: string, timezone: string}} param0.modifiedOn
+     * @param {number[]} param0.galleryIds
+     */
     constructor({id, filename, title, description, createdOn, modifiedOn, galleryIds}) {
         this.id = Number(id);
         this.filename = filename;
@@ -6,7 +16,6 @@ export default class Image {
         this.description = description;
         this.createdOn = new Date(createdOn.date + createdOn.timezone);
         this.modifiedOn = modifiedOn ? new Date(modifiedOn.date + modifiedOn.timezone) : undefined;
-        /** @type number[] */
         this.galleryIds = galleryIds.map(gId => Number(gId));
     }
 }
