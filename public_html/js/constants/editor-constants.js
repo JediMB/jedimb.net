5e2c3a365cbf1b3a8bfc22e0b6d906aedfb5511b
JediMB <jedimb@live.se>
2026-02-04 14:53:08 +0100

    Added transition- and wait-time attributes to whitelist in editor constants

--- public_html/js/constants/editor-constants.js
+++ public_html/js/constants/editor-constants.js
@@ -30,7 +30,7 @@ export const tagWhiteList = Object.freeze([...containerTags, ...textContentTags,
 export const allowedAttributes = Object.freeze({
     a: [ 'href', 'target', 'title' ],
     img: [ 'src', 'alt', 'data-image-id' ],
-    'img-gallery': [ 'gallery-id', 'aspect-ratio', 'width' ]
+    'img-gallery': [ 'gallery-id', 'aspect-ratio', 'width', 'transition-time', 'wait-time' ]
 });
 
 /** Array of uppercase keys that should have their default behavior even when accompanied by modifier keys
