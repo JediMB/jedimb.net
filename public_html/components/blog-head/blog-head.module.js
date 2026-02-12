8a52eb8380f574d5fe2a37f59216576466673df5
JediMB <jedimb@live.se>
2026-02-12 17:00:54 +0100

    Removed commented-out code, made input fields that need validation private members, and implemented basic validation checks on input

--- public_html/components/blog-head/blog-head.module.js
+++ public_html/components/blog-head/blog-head.module.js
@@ -3,19 +3,29 @@ import { TextEditorComponent } from "/js/components/text-editor/text-editor.modu
 
 customElements.define('blog-head-component', class BlogHeadComponent extends HTMLElement {
     #scheduledTimeout = { id: null };
+
+    /** @type {HTMLInputElement} */ #title;
+    /** @type {HTMLInputElement} */ #permalink;
+    /** @type {TextEditorComponent} */ #textEditor;
+    /** @type {HTMLInputElement} */ #description;
+    /** @type {HTMLInputElement} */ #socialLink;
+
+    #btnPublishPost;
+    #btnDraftPost;
+
     constructor() { super(); }
 
     connectedCallback() {
         const content = this.querySelector('blog-head-content');
 
         const body = content.querySelector('blog-head-body');
-        const title = body.querySelector('#blog-head__title');
+        this.#title = body.querySelector('#blog-head__title');
         const permadate = body.querySelector('#blog-head__permadate');
-        const permalink = body.querySelector('#blog-head__permalink');
+        this.#permalink = body.querySelector('#blog-head__permalink');
         const btnResetPermalink = body.querySelector('#blog-head__reset_permalink');
-        /** @type {TextEditorComponent} */ const textEditor = this.querySelector('#blog-head__text-editor');
-        const description = body.querySelector('#blog-head__description');
-        const sociallink = body.querySelector('#blog-head__sociallink');
+        this.#textEditor = this.querySelector('#blog-head__text-editor');
+        this.#description = body.querySelector('#blog-head__description');
+        this.#socialLink = body.querySelector('#blog-head__sociallink');
 
         const footer = content.querySelector('blog-head-footer');
         const tglPinned = footer.querySelector('#blog-head__toggle-pinned');
@@ -24,20 +34,19 @@ customElements.define('blog-head-component', class BlogHeadComponent extends HTM
         /** @type {HTMLInputElement} */ const scheduledTime = footer.querySelector('#blog-head__scheduled-time');
         const btnAddPost = footer.querySelector('#blog-head__btn-add');
         const btnCancelPost = footer.querySelector('#blog-head__btn-cancel');
-        const btnPublishPost = footer.querySelector('#blog-head__btn-publish');
-        const btnDraftPost = footer.querySelector('#blog-head__btn-draft');
+        this.#btnPublishPost = footer.querySelector('#blog-head__btn-publish');
+        this.#btnDraftPost = footer.querySelector('#blog-head__btn-draft');
 
-        title.addEventListener('input', () => permalink.defaultValue = this.#formatPermalinkTitle(title.value));
-        title.addEventListener('change', () => title.value = title.value.trim());
+        this.#title.addEventListener('input', () => this.#permalink.defaultValue = this.#formatPermalinkTitle(this.#title.value));
+        this.#title.addEventListener('change', () => this.#title.value = this.#title.value.trim());
 
-        permalink.addEventListener('change', () => permalink.value = this.#formatPermalinkTitle(permalink.value));
-        btnResetPermalink.addEventListener('click', () => permalink.value = permalink.defaultValue);
+        this.#permalink.addEventListener('change', () => this.#permalink.value = this.#formatPermalinkTitle(this.#permalink.value));
+        btnResetPermalink.addEventListener('click', () => this.#permalink.value = this.#permalink.defaultValue);
 
-        textEditor.content.onChange = () => {
-            const isEmpty = !textEditor.content.text.length;
-            btnPublishPost.disabled = isEmpty;
-            btnDraftPost.disabled = isEmpty;
+        for (const field of [this.#title, this.#permalink, this.#description, this.#socialLink]) {
+            field.addEventListener('input', () => this.#validation());
         }
+        this.#textEditor.content.onChange = () => this.#validation();
 
         tglScheduled.addEventListener('change', event => {
             const isScheduled = event.target.checked;
@@ -46,15 +55,15 @@ customElements.define('blog-head-component', class BlogHeadComponent extends HTM
             scheduledDate.toggleAttribute('hidden', !isScheduled);
             scheduledTime.toggleAttribute('required', isScheduled);
             scheduledTime.toggleAttribute('hidden', !isScheduled);
-            btnPublishPost.textContent = isScheduled ? btnPublishPost.dataset.contentSchedule : btnPublishPost.dataset.contentPublish;
+            this.#btnPublishPost.textContent = isScheduled ? this.#btnPublishPost.dataset.contentSchedule : this.#btnPublishPost.dataset.contentPublish;
             permadate.textContent = isScheduled ? scheduledDate.value.replaceAll('-', '/') : permadate.dataset.default;
 
             this.#updateDateTimeFields(scheduledDate, scheduledTime, isScheduled);
         });
 
-        btnPublishPost.addEventListener('click', () => {
-            console.log(textEditor.content.html, textEditor.content.text);
-            console.log(textEditor.content.media);
+        this.#btnPublishPost.addEventListener('click', () => {
+            console.log(this.#textEditor.content.html, this.#textEditor.content.text);
+            console.log(this.#textEditor.content.media);
         });
     }
 
@@ -69,38 +78,6 @@ customElements.define('blog-head-component', class BlogHeadComponent extends HTM
                 .replaceAll(/\-{2,}/g, '')
                 .replaceAll(/(^\-)|(\-$)/g, '');
     }
-
-    // /**
-    //  * @param {HTMLInputElement} dateInput 
-    //  * @param {HTMLInputElement} timeInput 
-    //  * @param {boolean} isScheduled
-    //  */
-    // #updateDateTimeFields(dateInput, timeInput, isScheduled) {
-    //     if (!isScheduled)
-    //         return clearTimeout(this.#scheduleTimeoutId);
-        
-    //     const now = new Date();
-        
-    //     const minTime = new Date(now.getTime() + 900000);
-    //     const followingHour = new Date(minTime.getTime() + 3600000);
-    //     const targetDateValue = `${followingHour.getFullYear()}-${`${followingHour.getMonth() + 1}`.padStart(2, '0')}-${`${followingHour.getDate()}`.padStart(2, '0')}`;
-
-    //     timeInput.min = `${minTime.getHours()}:${`${minTime.getMinutes()}`.padStart(2, '0')}`;
-    //     dateInput.min = targetDateValue;
-
-    //     const defaultTimeValue = `${followingHour.getHours()}:00`;
-
-    //     if (this.isScheduled
-    //         && timeInput.value === timeInput.defaultValue
-    //         && timeInput.defaultValue !== defaultTimeValue) {
-    //             console.log('Ding!');
-    //     }
-
-    //     timeInput.defaultValue = defaultTimeValue;
-    //     dateInput.defaultValue = targetDateValue;
-
-    //     this.#scheduleTimeoutId = setTimeout(this.#updateDateTimeFields, 60000, dateInput, timeInput, isScheduled);
-    // }
 
     /**
      * @param {HTMLInputElement} dateInput 
@@ -139,7 +116,16 @@ customElements.define('blog-head-component', class BlogHeadComponent extends HTM
         recursiveLogic(this.#scheduledTimeout);
     }
 
-    #isInvalid() {
+    #validation() {
+        const textEditorValid = this.#textEditor.content.text.length || this.#textEditor.content.media.length;
 
+        const valid = textEditorValid
+            && this.#title.checkValidity()
+            && this.#permalink.checkValidity()
+            && this.#description.checkValidity()
+            && this.#socialLink.checkValidity();
+
+        this.#btnPublishPost.disabled = !valid;
+        this.#btnDraftPost.disabled = !valid;
     }
 });
