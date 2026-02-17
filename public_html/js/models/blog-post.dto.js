9fff42c489d3915fcb174cd2811dd7c6cc363c3e
JediMB <jedimb@live.se>
2026-02-17 17:03:35 +0100

    Completed frontend BlogPostDTO constructor

--- public_html/js/models/blog-post.dto.js
+++ public_html/js/models/blog-post.dto.js
@@ -1,14 +1,17 @@
 export default class BlogPostDTO {
-    constructor(id, permalink, title, description, contentShort, contentRest,
-        mastolink, isPinned, scheduledOn) {
-        this.id = 0;
-        this.permalink = '';
-        this.title = '';
-        this.description = '';
-        this.contentShort = '';
-        this.contentRest = '';
-        this.mastolink = '';
-        this.isPinned = false;
-        /** @type {Date} */ this.scheduledOn = null;
+    /**
+     * @param {{ id: ?number, permalink: string, title: string, description: string, contentShort: string, contentRest: ?string, mastolink: ?string, isPinned: boolean, scheduledOn: ?Date }} param0 
+     */
+    constructor({id, permalink, title, description, contentShort, contentRest,
+        mastolink, isPinned, scheduledOn}) {
+        this.id = id ?? 0;
+        this.permalink = permalink;
+        this.title = title;
+        this.description = description;
+        this.contentShort = contentShort;
+        this.contentRest = contentRest ? contentRest : null;
+        this.mastolink = mastolink ? mastolink : null;
+        this.isPinned = isPinned;
+        this.scheduledOn = scheduledOn ?? null;
     }
 }
