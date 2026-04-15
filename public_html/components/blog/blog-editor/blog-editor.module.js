e2ca72ce3b33836f5ea3d84a98e444c0af2676be
JediMB <jedimb@live.se>
2026-04-15 16:54:34 +0200

    Added updateSaveTime, initial save/update functionality, and slightly refactored Blog Editor

--- public_html/components/blog/blog-editor/blog-editor.module.js
+++ public_html/components/blog/blog-editor/blog-editor.module.js
@@ -3,9 +3,15 @@ import BlogPostDTO from "/js/models/blog/blog-post.dto.model.js";
 import blogPostService from "/js/services/blog-post.service.js";
 
 export default class BlogEditorComponent extends HTMLElement {
-    /** @type {BlogFormComponent} */ #blogForm;
     #isChanged = false;
     #isValid = false;
+
+    /** @type {BlogFormComponent} */ #blogForm;
+    /** @type {HTMLSpanElement} */ #saveTime;
+
+    /** @type {HTMLButtonElement} */ #btnCancel;
+    /** @type {HTMLButtonElement} */ #btnSave;
+    /** @type {HTMLButtonElement} */ #btnPublish;
 
     constructor() { super(); }
 
@@ -13,30 +19,31 @@ export default class BlogEditorComponent extends HTMLElement {
         this.#blogForm = this.querySelector('#blog-editor__editor');
 
         const buttons = this.querySelector('edit-buttons');
-        const btnCancel = buttons.querySelector('#blog-editor__btn-cancel');
-        const btnPublish = buttons.querySelector('#blog-editor__btn-publish');
-        /** @type {HTMLButtonElement} */
-        const btnSave = buttons.querySelector('#blog-editor__btn-save');
+        this.#btnCancel = buttons.querySelector('#blog-editor__btn-cancel');
+        this.#btnPublish = buttons.querySelector('#blog-editor__btn-publish');
+        this.#btnSave = buttons.querySelector('#blog-editor__btn-save');
 
-        btnCancel.addEventListener('click', event => {
+        this.#saveTime = buttons.querySelector('[save-time]');
+
+        this.#btnCancel.addEventListener('click', event => {
             event.preventDefault();
             this.#cancel();
         });
 
-        btnPublish?.addEventListener('click', event => {
+        this.#btnPublish?.addEventListener('click', event => {
             event.preventDefault();
-            this.#publishOrSchedule();
+            this.#onPublish();
         });
 
         this.#blogForm.onSubmit(event => {
             event.preventDefault();
-            this.#save();
+            this.#onSave();
         });
 
         this.#blogForm.isChanged.subscribe({
             next: changed => {
                 this.#isChanged = changed;
-                btnSave.disabled = !changed || !this.#isValid;
+                this.#btnSave.disabled = !changed || !this.#isValid;
             }
         }, { getCurrent: true });
 
@@ -44,28 +51,28 @@ export default class BlogEditorComponent extends HTMLElement {
             next: valid => {
                 this.#isValid = valid;
 
-                if (btnPublish)
-                    btnPublish.disabled = !valid;
+                if (this.#btnPublish)
+                    this.#btnPublish.disabled = !valid;
 
-                btnSave.disabled = !valid || !this.#isChanged;
+                this.#btnSave.disabled = !valid || !this.#isChanged;
             }
         }, { getCurrent: true });
 
-        if (btnPublish) {
+        if (this.#btnPublish) {
             this.#blogForm.isScheduled.subscribe({
                 next: scheduled => {
-                    btnPublish.textContent = scheduled
-                        ? btnPublish.dataset.contentSchedule
-                        : btnPublish.dataset.contentPublish;
+                    this.#btnPublish.textContent = scheduled
+                        ? this.#btnPublish.dataset.contentSchedule
+                        : this.#btnPublish.dataset.contentPublish;
                 }
             }, { getCurrent: true });
         }
 
         this.#blogForm.onLoaded(() => {
-            if (btnPublish)
-                btnPublish.disabled = false;
+            if (this.#btnPublish)
+                this.#btnPublish.disabled = false;
 
-            btnCancel.disabled = false;
+            this.#btnCancel.disabled = false;
         });
     }
 
@@ -74,15 +81,13 @@ export default class BlogEditorComponent extends HTMLElement {
     disconnectedCallback() {}
 
     #cancel() {
-        const formData = this.#blogForm.getFormData();
-
-        if (formData.has('isPublished'))
-            window.location.assign(formData.get('isPublished'));
+        if (this.#blogForm.isPublished)
+            window.location.assign(this.#blogForm.publishedPath);
         else
             window.location.assign('/');
     }
 
-    #publishOrSchedule() {
+    #onPublish() {
         // Save changes and either publish or schedule publishing
         const post = new BlogPostDTO(this.#blogForm.getFormData());
 
@@ -108,8 +113,50 @@ export default class BlogEditorComponent extends HTMLElement {
         );
     }
 
-    #save() {
+    #onSave() {
         // Save changes but don't change its publishedOn value
+        this.#btnSave.disabled = true;
+        this.#btnSave.toggleAttribute('btn-loading', true);
+
+        if (this.#btnPublish)
+            this.#btnPublish.disabled = true;
+
+        const post = new BlogPostDTO(this.#blogForm.getFormData());
+
+        if (this.#blogForm.isPublished) {
+            blogPostService.updateBlogPost(post,
+                value => {
+
+                },
+                errors => {
+
+                }
+            );
+
+            return;
+        }
+
+        blogPostService.saveDraft(post,
+            value => {
+
+            },
+            errors => {
+
+            }
+        );
+    }
+
+    /** @param {Date} saveTime  */
+    #updateSaveTime(saveTime) {
+        const now = new Date();
+        const isSavedToday = saveTime.getDate() === now.getDate()
+            && saveTime.getMonth() === now.getMonth()
+            && saveTime.getFullYear() === now.getFullYear();
+
+        if (isSavedToday)
+            this.#saveTime.textContent = saveTime.toLocaleTimeString();
+        else
+            this.#saveTime.textContent = saveTime.toLocaleString();
     }
 }
 
