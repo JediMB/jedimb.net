e7803d7ea0b2425f598455a1480eaab4bce5057f
JediMB <jedimb@live.se>
2026-04-28 17:50:22 +0200

    Users with cofiguration permissions can now get hidden and/or unpublished posts by adding an 'admin' key to the API call

--- public_html/api/blog/posts.php
+++ public_html/api/blog/posts.php
@@ -1,9 +1,15 @@
 <?php declare(strict_types=1);
 
+require_once 'enums/blog-post-status.enum.php';
+require_once 'enums/published-status.enum.php';
+require_once 'enums/visibility.enum.php';
 require_once 'services/blog-post.service.php';
 require_once 'utilities/response.utility.php';
 
+use Enums\BlogPostStatus;
+use Enums\PublishedStatus;
 use Enums\UserPermission;
+use Enums\Visibility;
 use Services\BlogPostService;
 use Services\SessionService;
 use Utilities\Response;
@@ -16,12 +22,42 @@ $apiParams = $GLOBALS['api_params'];
 $page = isset($apiParams[0]) && is_numeric($apiParams[0]) ? (int)$apiParams[0] : 1;
 $pageSize = isset($apiParams[1]) && is_numeric($apiParams[1]) ? (int)$apiParams[1] : null;
 
+$adminKey = array_find_key($apiParams, fn($value) => $value === 'admin');
+
 $input = json_decode(file_get_contents('php://input'), true);
 
 switch ( $_SERVER['REQUEST_METHOD'] ) {
     case 'GET':
         try {
-            $result = $service->getPublicBlogPosts($page, $pageSize);
+            if (!is_int($adminKey) || !$sessionService->hasPermissions([ UserPermission::Configuration ])) {
+                $result = $service->getPublicBlogPosts($page, $pageSize);
+
+                return Response::Success($result);
+            }
+
+            $adminParams = array_slice($apiParams, $adminKey);
+
+            $publishedStatus = PublishedStatus::Published;
+
+            if (in_array(BlogPostStatus::Unpublished->value, $adminParams))
+                $publishedStatus = PublishedStatus::Unpublished;
+
+            if (in_array(BlogPostStatus::Published->value, $adminParams))
+                $publishedStatus = ($publishedStatus === PublishedStatus::Unpublished)
+                    ? PublishedStatus::Any
+                    : PublishedStatus::Published;
+
+            $visibility = Visibility::Visible;
+
+            if (in_array(BlogPostStatus::Hidden->value, $adminParams))
+                $visibility = Visibility::Hidden;
+            
+            if (in_array(BlogPostStatus::Visible->value, $adminParams))
+                $visibility = ($visibility === Visibility::Hidden)
+                    ? Visibility::Any
+                    : Visibility::Visible;
+
+            $result = $service->getBlogPostsAdminData($page, $pageSize, $publishedStatus, $visibility);
 
             return Response::Success($result);
         }
