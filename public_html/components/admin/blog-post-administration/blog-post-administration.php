9827a0f3f974d5492bb1f8fa1643c8e4575dfcc1
JediMB <jedimb@live.se>
2026-04-29 16:58:49 +0200

    Initial markup added for published status and visibility toggles

--- public_html/components/admin/blog-post-administration/blog-post-administration.php
+++ public_html/components/admin/blog-post-administration/blog-post-administration.php
@@ -2,9 +2,11 @@
 
 namespace Components\Admin;
 
+require_once 'enums/blog-post-status.enum.php';
 require_once 'services/blog-post.service.php';
 require_once 'utilities/component.utility.php';
 
+use Enums\BlogPostStatus;
 use Services\BlogPostService;
 use Utilities\Component;
 
@@ -20,6 +22,44 @@ Component::addJSModule();
 ?>
 
 <h3 class="h3">Posts</h3>
+
+<section>
+    <h4>Published status:</h4>
+    <label>
+        <input type="checkbox"
+            name="published-status"
+            value="<?= BlogPostStatus::Published->value ?>">
+        Published
+    </label>
+    <label>
+        <input type="checkbox"
+            name="published-status"
+            value="<?= BlogPostStatus::Unpublished->value ?>">
+        Unpublished
+    </label>
+</section>
+
+<section>
+    <h4>Visibility:</h4>
+    <label>
+        <input type="checkbox"
+            name="visibility"
+            value="<?= BlogPostStatus::Visible->value ?>">
+        Published
+    </label>
+    <label>
+        <input type="checkbox"
+            name="visibility"
+            value="<?= BlogPostStatus::Hidden->value ?>">
+        Unpublished
+    </label>
+</section>
+
+<div text-right>
+    Showing posts
+    <span id="blog__items-start"><?= $pagination->offset + 1 ?></span>&ndash;<span id="blog__items-end"><?= $pagination->offset + count($posts) ?></span>
+    (of <span id="blog__items-total"><?= $pagination->itemCount ?></span>)
+</div>
 
 <ul id="admin__blog-post__list">
     <?php foreach ($posts as $post): ?>
